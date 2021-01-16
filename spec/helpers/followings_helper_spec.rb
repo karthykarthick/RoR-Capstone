bcf21edf72b3895fe9a9fea22c0c1a02b973a1a5
karthick <karthylock@gmail.com>
2021-01-16 14:47:24 +0530

    updated tests

--- spec/helpers/followings_helper_spec.rb
+++ spec/helpers/followings_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe FollowingsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe FollowingsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
